c7ff15318d1848ad2e412516e895e3f4434d36e8
kwy404 <alexandre_rk_silva@estudante.sc.senai.br>
2022-04-04 21:27:19 -0300

    Chat float fix

--- src/pages/dashboard/chatFloat.js
+++ src/pages/dashboard/chatFloat.js
@@ -70,7 +70,7 @@ const Friend = (props) => {
                 <div className="avatarStack-3vfSFa">
                   <img 
                   style={{borderRadius: `50%`}}
-                  src="../assets/default.webp" alt=" " className="avatar-b5OQ1N" aria-hidden="true" />
+                  src={`${ typeof props.friend.photo != `object` ? props.friend.photo : window[`getPath`]() + `assets/default.webp`}`} alt=" " className="avatar-b5OQ1N" aria-hidden="true" />
                 </div>
               </foreignObject>
               <rect 

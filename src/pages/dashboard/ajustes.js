632108c196e2fb728cd0f2d03f09bace03a96af8
kwy404 <alexandre_rk_silva@estudante.sc.senai.br>
2022-04-04 21:34:00 -0300

    Navigate

--- src/pages/dashboard/ajustes.js
+++ src/pages/dashboard/ajustes.js
@@ -466,12 +466,13 @@ const Ajustes = props => {
                 <div className="item-3XjbnG selected-g-kMVV themed-2-lozF" role="tab" aria-selected="true" aria-controls="profile-customization-tab" aria-disabled="false" tabIndex={0} aria-label="Perfil do usuário">Perfil do usuário</div>
                 <div className="separator-2wx7h6" />
                 <div 
-                onClick={() => {
-                  window.localStorage.setItem(`token`, ``)
-                  return <Navigate to={`/`}/>
-                }}
                 className="item-3XjbnG themed-2-lozF" role="tab" aria-selected="false" aria-controls="logout-tab" aria-disabled="false" tabIndex={-1} aria-label="Sair">
-                  <div className="tabBarItemContainer-2HdIlr">Sair <svg aria-hidden="false" width={16} height={16} viewBox="0 0 24 24">
+                  <div 
+                  onClick={() => {
+                    window.localStorage.setItem(`token`, ``)
+                    return <Navigate to={`/`}/>
+                  }}
+                  className="tabBarItemContainer-2HdIlr">Sair <svg aria-hidden="false" width={16} height={16} viewBox="0 0 24 24">
                       <path fill="currentColor" d="M18 2H7C5.897 2 5 2.898 5 4V11H12.59L10.293 8.708L11.706 7.292L16.414 11.991L11.708 16.706L10.292 15.294L12.582 13H5V20C5 21.103 5.897 22 7 22H18C19.103 22 20 21.103 20 20V4C20 2.898 19.103 2 18 2Z" />
                     </svg>
                   </div>

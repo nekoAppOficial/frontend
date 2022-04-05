50cea0a773270b59625516c679497a472fa146db
kwy404 <alexandre_rk_silva@estudante.sc.senai.br>
2022-04-05 14:45:38 -0300

    Design

--- src/pages/dashboard/channelsMe.js
+++ src/pages/dashboard/channelsMe.js
@@ -18,7 +18,7 @@ const ChannelsMe = props => {
         <ul role="list" aria-label="Mensagens diretas" className="content-2a4AW9" style={{height: '408px'}}>
           <div aria-hidden="true" style={{height: '8px'}} />
           <li className="channel-1Shao0 container-32HW5s" role="listitem" aria-posinset={1} aria-setsize={8}>
-            <div className="interactive-1vLZ_I interactive-iyXY_x linkButton-2NshQj">
+            <div className={`interactive-1vLZ_I interactive-iyXY_x linkButton-2NshQj ${window.location.pathname == `/channels/@me` ? `interativoBtn` : ``}`}>
               <Link className="link-39sEB3" data-list-item-id="private-channels-uid_975___friends" tabIndex={-1} to="/channels/@me">
                 <div className="layout-1LjVue">
                   <div className="avatar-1HDIsL">
@@ -29,7 +29,8 @@ const ChannelsMe = props => {
                       </g>
                     </svg>
                   </div>
-                  <div className="content-66wMin">
+                  <div 
+                  className="content-66wMin">
                     <div className="nameAndDecorators-2A8Bbk">
                       <div className="name-2m3Cms">Amigos </div>
                     </div>
